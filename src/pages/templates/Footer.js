d92c3bab85ad1d941759dec8ad60cc7f32aa01f4
Jon Weber <jonweber09@gmail.com>
2019-04-20 15:32:11 -0700

    added content to footer

--- src/pages/templates/Footer.js
+++ src/pages/templates/Footer.js
@@ -4,9 +4,51 @@ class Footer extends Component {
 	render() {
 		return(
 			<div className="footer">
-				Resumé
-				GitHub
-				LinkedIn
+				<div className="row">
+					<div className="col-1-of-2">
+						<h4 className="footer__header">About me</h4>
+
+						<div className="footer__about">
+							<img src="" alt="Jon"/>
+
+							<p>My name is Jon and I am a full-time Web Developer based in Vista, CA. 
+							I specialize in creating dynamic and beautiful web pages and apps using the 
+							latest technologies and tools. I have been in the field for around 8 years, 
+							and still get excited about new projects and learning new things. I’m a developer, 
+							a designer, a thinker, a creative, and have an eye for detail. Check out some 
+							of my footer links to see what I’ve been up to lately.</p>
+						</div>
+					</div>
+					<div className="col-1-of-2">
+						<h4 className="footer__header">Professional Resources</h4>
+						<div className="footer__links">
+							<div className="footer__links-single">
+								<a href="" target="_blank">
+									<img src="" alt="Resumé"/>
+								</a>
+								<p>Resumé</p>
+							</div>
+							<div className="footer__links-single">
+								<a href="" target="_blank">
+									<img src="" alt="GitHub"/>
+								</a>
+								<p>GitHub</p>
+							</div>
+							<div className="footer__links-single">
+								<a href="" target="_blank">
+									<img src="" alt="LinkedIn"/>
+								</a>
+								<p>LinkedIn</p>
+							</div>
+						</div>
+						<div className="footer__badges">
+							<img src="" alt="HTML5 Badge"/>
+							<img src="" alt="CSS3 Badge"/>
+							<img src="" alt="Javascript 6 Badge"/>
+						</div>
+					</div>
+				</div>
+				
 			</div>
 		);
 	}
