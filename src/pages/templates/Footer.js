8398bf55d39b79e4e9556da10627bb47f8bbbd88
JayWebz <jweber@caida.org>
2021-09-29 10:58:33 -0700

    update text content on caida page

--- src/pages/templates/Footer.js
+++ src/pages/templates/Footer.js
@@ -12,11 +12,11 @@ class Footer extends Component {
 						<div className="footer__about">
 							<img src={process.env.PUBLIC_URL + "/images/JonWeber.jpg"} alt="Me"/>
 
-							<p>My name is Jon and I am a full-time Web Developer based in Vista, CA. 
-							I specialize in creating dynamic and beautiful web pages and apps using the 
-							latest technologies and tools. I have been in the field for around 7 years, 
-							and still get excited about new projects and learning new things. I’m a developer, 
-							a designer, a thinker, a creative, and have an eye for detail. Check out 
+							<p>My name is Jon and I am a full-time Web Developer based in Vista, CA.
+							I specialize in creating dynamic and beautiful web pages and apps using the
+							latest technologies and tools. I have been in the field since 2012,
+							and still get excited about new projects and learning new things. I’m a developer,
+							a designer, a thinker, a creative, and have an eye for detail. Check out
 							my GitHub or message me to see what I’ve been up to lately.</p>
 						</div>
 					</div>
